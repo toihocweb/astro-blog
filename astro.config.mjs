7a315dc4e68dab187ef2d81cd6204299f3831cdd
Nath <dinhnhat.bui@zuhlke.com>
2023-01-25 12:33:10 +0700

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from "astro/config";
+import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), image(), mdx(), sitemap()]
+});
